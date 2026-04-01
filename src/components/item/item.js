ca3e1f46d7eeb46a2112bbac771c666d3155b03a
code <code@Adantas-MacBook-Air.local>
2026-04-01 12:59:37 -0400

    uploaded ex imgs

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -24,22 +24,24 @@ function Item(){
                 <article>
 
                     <figure>
-                        <img alt="" src={details.image}/>
+                        <img alt="" src={details.image} height="100%"/>
                         <figcaption>
                         level: {details.level} | </figcaption>
                     </figure>
 
-                    <div className={styles.target}>
-                        <h3>Muscles Targetted</h3>
-                        <span className={styles.heavy}>Primary</span>: {details?.muscles?.primary?.join('- ')}<br/>
-                        <span className={styles.heavy}>Secondary</span>: {details?.muscles?.secondary?.join('- ')}
-                    </div>
+                    <div className={styles.itemDetail}>
+                        <div className={styles.target}>
+                            <h3>Muscles Targetted</h3>
+                            <span className={styles.heavy}>Primary</span>: {details?.muscles?.primary?.join('- ')}<br/>
+                            <span className={styles.heavy}>Secondary</span>: {details?.muscles?.secondary?.join('- ')}
+                        </div>
 
-                    <div className={styles.howTo}>
-                        <h3>how to perform</h3>
-                        <p>
-                            {details.execution?.join(' ')}
-                        </p>
+                        <div className={styles.howTo}>
+                            <h3>how to perform</h3>
+                            <p>
+                                {details.execution?.join(' ')}
+                            </p>
+                        </div>
                     </div>
 
                 </article>
